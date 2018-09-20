598632ed459440ef1a1952f339f5e67451414da7
Lluís Gómez Hernando <lluisgh28@gmail.com>
2018-09-20 10:52:30 +0200

    Add app project

---
+++ RandomUser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomUser
+//
+//  Created by Lluís Gómez on 20/09/2018.
+//  Copyright © 2018 RandomUser Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
