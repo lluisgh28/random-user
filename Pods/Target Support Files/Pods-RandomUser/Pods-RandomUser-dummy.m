fa80460ba9c568f04bdd7b503bd4f4bfcad37b40
lluisgh28 <lluisgh28@gmail.com>
2018-09-26 11:24:32 +0200

    Improve .gitignore

--- Pods/Target Support Files/Pods-RandomUser/Pods-RandomUser-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RandomUser : NSObject
-@end
-@implementation PodsDummy_Pods_RandomUser
-@end
--- Pods/Target Support Files/Pods-RandomUserDomainKit-RandomUserDomainKitTests/Pods-RandomUserDomainKit-RandomUserDomainKitTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RandomUserDomainKit_RandomUserDomainKitTests : NSObject
-@end
-@implementation PodsDummy_Pods_RandomUserDomainKit_RandomUserDomainKitTests
-@end
