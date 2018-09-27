ef83c0c4df1d1bc3d6201109324d69196c88bca2
lluisgh28 <lluisgh28@gmail.com>
2018-09-27 18:35:03 +0200

    Make UserList Module the root one

---
+++ RandomUser/AppFactory.swift
@@ -0,0 +1,13 @@
+//
+//  AppFactory.swift
+//  RandomUser
+//
+//  Created by Lluis Gomez Hernando on 27/09/2018.
+//  Copyright © 2018 RandomUser Inc. All rights reserved.
+//
+
+import Foundation
+
+class AppFactory {
+    lazy var userListModuleFactory = UserListModuleFactory()
+}
---
+++ RandomUser/Modules/User List/UserListModuleFactory.swift
@@ -0,0 +1,17 @@
+//
+//  UserListModuleFactory.swift
+//  RandomUser
+//
+//  Created by Lluis Gomez Hernando on 27/09/2018.
+//  Copyright © 2018 RandomUser Inc. All rights reserved.
+//
+
+import UIKit
+
+class UserListModuleFactory {
+    
+    func makeViewController() -> UIViewController {
+        
+        return UIViewController()
+    }
+}
